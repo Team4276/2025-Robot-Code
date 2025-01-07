ebe77c502c18e86df93f7d271b118006adb586d2
Breadfest <ethanoskatt1@gmail.com>
2025-01-06 19:03:59 -0800

    add axisalign controller; add field elements

--- Comp/src/main/java/frc/team4276/frc2025/subsystems/drive/Drive.java
+++ Comp/src/main/java/frc/team4276/frc2025/subsystems/drive/Drive.java
@@ -34,6 +34,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.team4276.frc2025.Constants;
 import frc.team4276.frc2025.Constants.Mode;
+import frc.team4276.frc2025.field.FieldConstants;
 import frc.team4276.frc2025.RobotState;
 import frc.team4276.frc2025.subsystems.drive.controllers.AutoAlignController;
 import frc.team4276.frc2025.subsystems.drive.controllers.HeadingController;
