962da010e2ffad546b2693332eb7070e0ef1c4fb
Breadfest <ethanoskatt1@gmail.com>
2025-03-01 08:51:49 -0800

    regen autos

--- Comp/src/main/java/frc/team4276/frc2025/Robot.java
+++ Comp/src/main/java/frc/team4276/frc2025/Robot.java
@@ -1,5 +1,6 @@
 package frc.team4276.frc2025;
 
+import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.Alert;
 import edu.wpi.first.wpilibj.Alert.AlertType;
 import edu.wpi.first.wpilibj.DriverStation;
@@ -120,6 +121,8 @@ public class Robot extends LoggedRobot {
     // Instantiate our RobotContainer. This will perform all our button bindings,
     // and put our autonomous chooser on the dashboard.
     robotContainer = new RobotContainer();
+
+    CameraServer.startAutomaticCapture();
   }
 
   /** This function is called periodically during all modes. */
