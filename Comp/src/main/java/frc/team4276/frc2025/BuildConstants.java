db7055f81808a9cf0d4ffd7e4617a7230c8135b8
eBulosan <ethanoskatt1@gmail.com>
2025-03-07 00:30:41 -0800

    i really need sleep; ok i promise tis is the last one

--- Comp/src/main/java/frc/team4276/frc2025/BuildConstants.java
+++ Comp/src/main/java/frc/team4276/frc2025/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Comp";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 179;
-  public static final String GIT_SHA = "c272b139bc847e1d7a39a6b7302996ba582d6c29";
-  public static final String GIT_DATE = "2025-03-07 03:27:59 EST";
+  public static final int GIT_REVISION = 180;
+  public static final String GIT_SHA = "1d2e954016eb2704efb48c2a58529ac911b7e843";
+  public static final String GIT_DATE = "2025-03-07 03:30:11 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-03-07 03:29:32 EST";
-  public static final long BUILD_UNIX_TIME = 1741336172232L;
+  public static final String BUILD_DATE = "2025-03-07 03:30:26 EST";
+  public static final long BUILD_UNIX_TIME = 1741336226971L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
