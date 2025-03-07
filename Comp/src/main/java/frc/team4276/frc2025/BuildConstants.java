5ff95fc626e9dd273e5037b20635e0d12e41e888
eBulosan <ethanoskatt1@gmail.com>
2025-03-07 00:30:41 -0800

    fix last merge; add todo

--- Comp/src/main/java/frc/team4276/frc2025/BuildConstants.java
+++ Comp/src/main/java/frc/team4276/frc2025/BuildConstants.java
@@ -9,8 +9,8 @@ public final class BuildConstants {
   public static final String GIT_SHA = "cf7f5713b1d69c0198dc3b1cadc1326d2c2dee4a";
   public static final String GIT_DATE = "2025-03-05 21:29:41 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-03-08 13:10:08 EST";
-  public static final long BUILD_UNIX_TIME = 1741457408133L;
+  public static final String BUILD_DATE = "2025-03-07 03:30:26 EST";
+  public static final long BUILD_UNIX_TIME = 1741336226971L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
