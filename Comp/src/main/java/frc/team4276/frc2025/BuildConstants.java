89d180918129fdef2b23bc551cb7fad8f8a5348d
Breadfest <ethanoskatt1@gmail.com>
2025-03-14 11:21:27 -0700

    update elastic

--- Comp/src/main/java/frc/team4276/frc2025/BuildConstants.java
+++ Comp/src/main/java/frc/team4276/frc2025/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Comp";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 201;
-  public static final String GIT_SHA = "9657fbc75eac7f036eb060cf9f032d2e6855ac3b";
-  public static final String GIT_DATE = "2025-03-14 03:13:36 GMT-04:00";
+  public static final int GIT_REVISION = 202;
+  public static final String GIT_SHA = "6b7c56c05baa743f3bbd6fa5b1ee5e1ceed19b70";
+  public static final String GIT_DATE = "2025-03-14 12:39:45 GMT-04:00";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-03-14 12:26:24 GMT-04:00";
-  public static final long BUILD_UNIX_TIME = 1741969584506L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-03-14 12:43:16 GMT-04:00";
+  public static final long BUILD_UNIX_TIME = 1741970596315L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
