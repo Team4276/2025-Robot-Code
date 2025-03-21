04c3bdc88a17d9f6978682d287fce82143d07af5
boden1203 <81496057+joe3925@users.noreply.github.com>
2025-03-21 13:18:04 -0700

    change Constants

--- Comp/src/main/java/frc/team4276/frc2025/BuildConstants.java
+++ Comp/src/main/java/frc/team4276/frc2025/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Comp";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 233;
-  public static final String GIT_SHA = "443d54b2cb0b89e25ba0d53e0236cc796df3f9c4";
-  public static final String GIT_DATE = "2025-03-21 14:45:16 GMT-04:00";
+  public static final int GIT_REVISION = 234;
+  public static final String GIT_SHA = "e56373ddfe92598e7e358997b1b94df42c3692dc";
+  public static final String GIT_DATE = "2025-03-21 15:00:27 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-03-21 14:51:29 GMT-04:00";
-  public static final long BUILD_UNIX_TIME = 1742583089425L;
+  public static final String BUILD_DATE = "2025-03-21 15:37:41 EDT";
+  public static final long BUILD_UNIX_TIME = 1742585861178L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
--- Comp/src/main/java/frc/team4276/frc2025/Constants.java
+++ Comp/src/main/java/frc/team4276/frc2025/Constants.java
@@ -44,7 +44,7 @@ public final class Constants {
         : "Robot must be in REAL mode when SysIdMode is enabled.";
   }
 
-  public static final boolean isTuning = true;
+  public static final boolean isTuning = false;
 
   public static final boolean isSim = (mode == Mode.SIM);
 
