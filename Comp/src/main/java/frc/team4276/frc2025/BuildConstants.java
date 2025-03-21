5d930e3ca73bf457bf3d91f6292efbb3d9bdc230
Breadfest <ethanoskatt1@gmail.com>
2025-03-21 13:42:15 -0700

    version for pm11

--- Comp/src/main/java/frc/team4276/frc2025/BuildConstants.java
+++ Comp/src/main/java/frc/team4276/frc2025/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Comp";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 234;
-  public static final String GIT_SHA = "e56373ddfe92598e7e358997b1b94df42c3692dc";
-  public static final String GIT_DATE = "2025-03-21 15:00:27 EDT";
+  public static final int GIT_REVISION = 235;
+  public static final String GIT_SHA = "04c3bdc88a17d9f6978682d287fce82143d07af5";
+  public static final String GIT_DATE = "2025-03-21 16:18:04 GMT-04:00";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-03-21 15:37:41 EDT";
-  public static final long BUILD_UNIX_TIME = 1742585861178L;
+  public static final String BUILD_DATE = "2025-03-21 16:41:05 GMT-04:00";
+  public static final long BUILD_UNIX_TIME = 1742589665688L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
