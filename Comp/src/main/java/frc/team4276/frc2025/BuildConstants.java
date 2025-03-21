e56373ddfe92598e7e358997b1b94df42c3692dc
Breadfest <ethanoskatt1@gmail.com>
2025-03-21 12:00:27 -0700

    increase align distance offset

--- Comp/src/main/java/frc/team4276/frc2025/BuildConstants.java
+++ Comp/src/main/java/frc/team4276/frc2025/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Comp";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 232;
-  public static final String GIT_SHA = "e429d67650118f9cd65412a5b9789e1216231e90";
-  public static final String GIT_DATE = "2025-03-21 02:44:17 EDT";
+  public static final int GIT_REVISION = 233;
+  public static final String GIT_SHA = "443d54b2cb0b89e25ba0d53e0236cc796df3f9c4";
+  public static final String GIT_DATE = "2025-03-21 14:45:16 GMT-04:00";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-03-21 14:33:15 EDT";
-  public static final long BUILD_UNIX_TIME = 1742581995212L;
+  public static final String BUILD_DATE = "2025-03-21 14:51:29 GMT-04:00";
+  public static final long BUILD_UNIX_TIME = 1742583089425L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
