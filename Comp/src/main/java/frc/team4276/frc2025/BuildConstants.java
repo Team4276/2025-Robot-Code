7fb41e12547b414329ece1c7ddbbae9294718642
eBulosan <ethanoskatt1@gmail.com>
2025-06-17 21:39:51 -0700

    fix constants

--- Comp/src/main/java/frc/team4276/frc2025/BuildConstants.java
+++ Comp/src/main/java/frc/team4276/frc2025/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Comp";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 266;
-  public static final String GIT_SHA = "9f63b28ede110dfa333a3b4ad07ea617a2408f76";
-  public static final String GIT_DATE = "2025-06-16 22:27:37 EDT";
+  public static final int GIT_REVISION = 267;
+  public static final String GIT_SHA = "9e2cab05e1b8e9f9d680f2b7542f723f12123ed6";
+  public static final String GIT_DATE = "2025-06-18 00:38:39 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-06-18 00:33:22 EDT";
-  public static final long BUILD_UNIX_TIME = 1750221202529L;
+  public static final String BUILD_DATE = "2025-06-18 00:38:58 EDT";
+  public static final long BUILD_UNIX_TIME = 1750221538676L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
