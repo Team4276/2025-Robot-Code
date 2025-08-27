412b133f99249f60932b5dca13ab0e676de9f8b8
eBulosan <122693686+eBulosan@users.noreply.github.com>
2025-08-26 19:36:33 -0700

    update vendordeps

--- Comp/src/main/java/frc/team4276/frc2025/BuildConstants.java
+++ Comp/src/main/java/frc/team4276/frc2025/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Comp";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 273;
-  public static final String GIT_SHA = "ec7d640e33e4b3808751bf8f1bc5fc747e517457";
-  public static final String GIT_DATE = "2025-07-17 19:05:33 GMT-04:00";
+  public static final int GIT_REVISION = 276;
+  public static final String GIT_SHA = "6986145ac2948d520e31d3190abc949c2ddcebdb";
+  public static final String GIT_DATE = "2025-07-24 14:30:48 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-07-23 20:36:52 GMT-04:00";
-  public static final long BUILD_UNIX_TIME = 1753317412940L;
+  public static final String BUILD_DATE = "2025-08-26 22:31:30 EDT";
+  public static final long BUILD_UNIX_TIME = 1756261890852L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
